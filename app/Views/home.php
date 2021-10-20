d81d04f2e72e2429779b979a2c059c3e24c0a655
MaximePag <max_p@live.fr>
2021-10-20 14:13:09 +0200

    fin accueil

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,1 +1,103 @@
-chshjlksdcslmdfjdskopkoflkdpoijkgyjhtgf
+<section id="home" class="container-fluid">
+    <div id="home_search" class="row">
+        <div class="col-lg-5 mx-auto">
+            <div id="card_search" class="card mx-3">
+                <div class="card-header">
+                    <h2 class="text-center mb-3">Recherchez votre futur bien</h2>
+                    <ul class="nav nav-pills nav-justified">
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active" aria-current="page" href="#">Achetez</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active" href="#">Louez</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link active" href="#">Vendez</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="Localisation_icon"><i class="fas fa-map-marked-alt fa-2x"></i></span>
+                        <input type="text" class="form-control" placeholder="Localisation" aria-label="Localisation" aria-describedby="Localisation_icon">
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="Budget_icon"><i class="fas fa-wallet fa-2x"></i></span>
+                        <input type="text" class="form-control" placeholder="Votre Budget" aria-label="Votre Budget" aria-describedby="Budget_icon">
+                        <span class="input-group-text"><i class="fas fa-euro-sign fa-1x"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="home_slider" class="row">
+        <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="card-group">
+                        <div class="card mx-3">
+                            <img src="/assets/images/maison-2.jpg" class="card-img-top" alt="/assets/images/maison-2.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card mx-3">
+                            <img src="/assets/images/maison-3.jpg" class="card-img-top" alt="/assets/images/maison-3.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card mx-3">
+                            <img src="/assets/images/maison-1.jpg" class="card-img-top" alt="/assets/images/maison-1.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item mb-2">
+                    <div class="card-group">
+                        <div class="card mx-3">
+                            <img src="/assets/images/maison-3.jpg" class="card-img-top" alt="/assets/images/maison-3.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card mx-3">
+                            <img src="/assets/images/maison-1.jpg" class="card-img-top" alt="lel">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card mx-3">
+                            <img src="/assets/images/maison-2.jpg" class="card-img-top" alt="/assets/images/maison-2.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</section>
